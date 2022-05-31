93e3ea24d19d8dc69fda77f8e55a67efb11f5152
synkarius <david@explosionduck.com>
2022-05-30 17:05:05 -0700

    SLEIGHT-8: selector visually complete

--- src/features/model/selector/SelectorComponent.tsx
+++ src/features/model/selector/SelectorComponent.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { useAppDispatch } from '../../../app/hooks';
+import React, { useId } from 'react';
+import { FormGroup, FormLabel, FormText, Row } from 'react-bootstrap';
+import { ArrayPositionType } from '../../../util/bootstrap-util';
 import { Selector } from './selector';
-import { createNewSelectorItem } from './selector-reducers';
 import { SelectorItemComponent } from './SelectorItemComponent';
 
 export const SelectorComponent: React.FC<{ selector: Selector }> = (props) => {
-  const dispatch = useAppDispatch();
-
-  const addHandler = (_event: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(createNewSelectorItem(props.selector.id));
-  };
+  const selectorFormGroupId = useId();
 
   return (
-    <>
-      {props.selector.items.map((selectorItem) => (
+    <FormGroup as={Row} className="mb-3" controlId={selectorFormGroupId}>
+      <FormLabel>Selector</FormLabel>
+      {props.selector.items.map((selectorItem, index) => (
         <SelectorItemComponent
           key={selectorItem.id}
           selectorId={props.selector.id}
           selectorItem={selectorItem}
+          arrayPositionType={ArrayPositionType.getArrayPositionType(
+            props.selector.items.length,
+            index
+          )}
         />
       ))}
-      <Button className="mb-3" onClick={addHandler} variant="primary">
-        Add Alternate
-      </Button>
-    </>
+      <FormText className="text-muted">selector (what to say)</FormText>
+    </FormGroup>
   );
 };
--- src/features/model/selector/SelectorItemComponent.tsx
+++ src/features/model/selector/SelectorItemComponent.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Button, FormControl, InputGroup } from 'react-bootstrap';
 import { useAppDispatch } from '../../../app/hooks';
+import { ArrayPositionType } from '../../../util/bootstrap-util';
 import { SelectorItem } from './selector';
-import { deleteSelectorItem, editSelectorItem } from './selector-reducers';
+import {
+  createNewSelectorItem,
+  deleteSelectorItem,
+  editSelectorItem,
+} from './selector-reducers';
 
 export const SelectorItemComponent: React.FC<{
   selectorId: string;
   selectorItem: SelectorItem;
   key: string;
+  arrayPositionType: string;
 }> = (props) => {
   const dispatch = useAppDispatch();
 
+  const addHandler = (_event: React.MouseEvent<HTMLButtonElement>) => {
+    dispatch(createNewSelectorItem(props.selectorId));
+  };
   const changedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(
       editSelectorItem({
@@ -29,16 +38,26 @@ export const SelectorItemComponent: React.FC<{
     );
   };
 
+  const marginBottom =
+    props.arrayPositionType === ArrayPositionType.LAST ? 'mb-0' : 'mb-3';
+
   return (
-    <InputGroup className="mb-3">
+    <InputGroup className={marginBottom}>
       <FormControl
         type="text"
         onChange={changedHandler}
         value={props.selectorItem.value}
       />
-      <Button variant="outline-secondary" onClick={deleteHandler}>
-        Remove
-      </Button>
+      {props.arrayPositionType === ArrayPositionType.FIRST && (
+        <Button variant="outline-primary" onClick={addHandler}>
+          Add Alternate
+        </Button>
+      )}
+      {props.arrayPositionType !== ArrayPositionType.FIRST && (
+        <Button variant="outline-secondary" onClick={deleteHandler}>
+          Remove
+        </Button>
+      )}
     </InputGroup>
   );
 };
