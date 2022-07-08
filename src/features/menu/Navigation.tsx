577810b7cda3305b223b2b867ab1734bf18fb2dd
synkarius <david@explosionduck.com>
2022-07-08 15:40:57 -0700

    SLEIGHT-11: disable everything on menu that isn't working yet

--- src/features/menu/Navigation.tsx
+++ src/features/menu/Navigation.tsx
@@ -9,43 +9,43 @@ export const Navigation = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="File" id="file-dropdown">
-              <NavDropdown.Item href="#action/3.1">
+              <NavDropdown.Item href="#action/3.1" disabled>
                 New Element Set
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item href="#action/3.2" disabled>
                 Open Element Set
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">
+              <NavDropdown.Item href="#action/3.3" disabled>
                 Import Element Set
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="#action/3.4" disabled>
                 Export Element Set
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.5">
+              <NavDropdown.Item href="#action/3.5" disabled>
                 Export Caster
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.6">
+              <NavDropdown.Item href="#action/3.6" disabled>
                 Export Dragonfly
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.7">
+              <NavDropdown.Item href="#action/3.7" disabled>
                 Export Talon
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.8">
+              <NavDropdown.Item href="#action/3.8" disabled>
                 Export Vocola 3
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Edit" id="edit-dropdown">
-              <NavDropdown.Item href="#action/4.1">
+              <NavDropdown.Item href="#action/4.1" disabled>
                 Duplicate Selected Element
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/4.2">
+              <NavDropdown.Item href="#action/4.2" disabled>
                 Delete Selected Element
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/4.3">
+              <NavDropdown.Item href="#action/4.3" disabled>
                 Preferences
               </NavDropdown.Item>
             </NavDropdown>
@@ -53,23 +53,25 @@ export const Navigation = () => {
               <NavDropdown.Item href="#action/6.1">
                 Editor Mode
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/6.2">
+              <NavDropdown.Item href="#action/6.2" disabled>
                 Wizard Mode
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Help" id="help-dropdown">
-              <NavDropdown.Item href="#action/5.1">
+              <NavDropdown.Item href="#action/5.1" disabled>
                 Documentation
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/5.2">
+              <NavDropdown.Item href="#action/5.2" disabled>
                 Release Notes
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/5.3">
+              <NavDropdown.Item href="#action/5.3" disabled>
                 View License
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/5.4">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/5.4" disabled>
+                About
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
