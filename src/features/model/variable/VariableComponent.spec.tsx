eec65144a92a390a382f6cc6b0126767abcbd78e
synkarius <david@explosionduck.com>
2022-08-28 22:00:14 -0700

    SLEIGHT-31: functional test for var used change type validator

--- src/features/model/variable/VariableComponent.spec.tsx
+++ src/features/model/variable/VariableComponent.spec.tsx
@@ -11,34 +11,51 @@ import { appDefaultInjectionContext } from '../../../app-default-injection-conte
 import { createRangeVariable } from './data/variable';
 import { getVariableDomainMapper } from './data/variable-domain-mapper';
 import { saveEditingVariable } from './variable-reducers';
+import { createPauseAction, PauseAction } from '../action/pause/pause';
+import { ActionValueType } from '../action/action-value/action-value-type';
+import { saveAction } from '../action/action-reducers';
 
 const SAVE = 'Save';
+const VARIABLE_1_ID = 'VARIABLE_1_ID';
 const VARIABLE_1_NAME = 'VARIABLE_1_NAME';
+const VARIABLE_2_ID = 'VARIABLE_2_ID';
+const VARIABLE_2_NAME = 'VARIABLE_2_NAME';
 
 let user: UserEvent;
 
 beforeAll(() => {
-  const variable = {
+  const variable1 = {
     ...createRangeVariable(),
+    id: VARIABLE_1_ID,
     name: VARIABLE_1_NAME,
   };
-  const variableDTO = getVariableDomainMapper().mapFromDomain(variable);
-  store.dispatch(saveEditingVariable(variableDTO));
+  const variable1DTO = getVariableDomainMapper().mapFromDomain(variable1);
+  store.dispatch(saveEditingVariable(variable1DTO));
+  const variable2 = {
+    ...createRangeVariable(),
+    id: VARIABLE_2_ID,
+    name: VARIABLE_2_NAME,
+  };
+  const variable2DTO = getVariableDomainMapper().mapFromDomain(variable2);
+  store.dispatch(saveEditingVariable(variable2DTO));
+  const action: PauseAction = {
+    ...createPauseAction(),
+    centiseconds: {
+      variableType: VariableType.Enum.RANGE,
+      actionValueType: ActionValueType.Enum.USE_VARIABLE,
+      variableId: variable1.id,
+    },
+  };
+  store.dispatch(saveAction(action));
   user = userEvent.setup();
 });
 
-beforeEach(async () => {
-  render(
-    <Provider store={store}>
-      <InjectionContext.Provider value={appDefaultInjectionContext}>
-        <VariableParentComponent />
-      </InjectionContext.Provider>
-    </Provider>
-  );
-});
+beforeEach(async () => {});
 
 describe('variable component tests', () => {
   it('should have a placeholder name', () => {
+    doRender();
+
     const nameField = screen.getByRole<HTMLInputElement>('textbox', {
       name: Field[Field.VAR_NAME],
     });
@@ -50,6 +67,8 @@ describe('variable component tests', () => {
   });
 
   it('should not save if validation errors', async () => {
+    doRender();
+
     const typeSelect = screen.getByRole('list', {
       name: Field[Field.VAR_TYPE_SELECT],
     });
@@ -61,6 +80,8 @@ describe('variable component tests', () => {
   });
 
   it('should invalidate an already taken name', async () => {
+    doRender();
+
     const nameField = screen.getByRole<HTMLInputElement>('textbox', {
       name: Field[Field.VAR_NAME],
     });
@@ -69,4 +90,45 @@ describe('variable component tests', () => {
 
     expect(nameField).toHaveClass('is-invalid');
   });
+
+  it('should invalidate type change if variable is used', async () => {
+    doRender(VARIABLE_1_ID);
+
+    const typeSelect = screen.getByRole('list', {
+      name: Field[Field.VAR_TYPE_SELECT],
+    });
+    await user.selectOptions(typeSelect, VariableType.Enum.CHOICE);
+
+    const errorText = screen.getByText(getVarUsedErrorMsgRegex());
+
+    expect(typeSelect).toHaveClass('is-invalid');
+    expect(errorText).toBeInTheDocument();
+  });
+
+  it('should validate type change if variable is unused', async () => {
+    doRender(VARIABLE_2_ID);
+
+    const typeSelect = screen.getByRole('list', {
+      name: Field[Field.VAR_TYPE_SELECT],
+    });
+    await user.selectOptions(typeSelect, VariableType.Enum.CHOICE);
+
+    const errorText = screen.queryByText(getVarUsedErrorMsgRegex());
+
+    expect(typeSelect).not.toHaveClass('is-invalid');
+    expect(errorText).not.toBeInTheDocument();
+  });
 });
+
+const getVarUsedErrorMsgRegex = () =>
+  /this variable is currently used \(in the action\(s\) ".*"\), so its type cannot be changed/;
+
+const doRender = (variableId?: string) => {
+  render(
+    <Provider store={store}>
+      <InjectionContext.Provider value={appDefaultInjectionContext}>
+        <VariableParentComponent variableId={variableId} />
+      </InjectionContext.Provider>
+    </Provider>
+  );
+};
