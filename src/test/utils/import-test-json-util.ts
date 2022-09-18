d8dccbe19307a75f9c20184aa7090e5669e0b2b8
synkarius <david@explosionduck.com>
2022-09-18 16:53:04 -0700

    SLEIGHT-66: begin reworking failing tests for better maintainability

---
+++ src/test/utils/import-test-json-util.ts
@@ -0,0 +1,2 @@
+/** Test utility function: casts imported json. */
+export const castJsonForTest = <T>(jsonObject: any): T => jsonObject as T;
--- src/ui/model/spec/SpecComponent.spec.tsx
+++ src/ui/model/spec/SpecComponent.spec.tsx
@@ -7,200 +7,76 @@ import { Field } from '../../../validation/validation-field';
 import { SpecParentComponent } from './SpecParentComponent';
 import { SpecItemType } from '../../../data/model/spec/spec-item-type';
 import { saveVariable } from '../../../core/reducers/variable-reducers';
-import {
-  createRangeVariable,
-  RangeVariable,
-} from '../../../data/model/variable/variable';
 import { InjectionContext } from '../../../di/injector-context';
 import { getDefaultInjectionContext } from '../../../di/app-default-injection-context';
 import { saveSpec } from '../../../core/reducers/spec-reducers';
-import {
-  createSpecItem,
-  Spec,
-  SpecItem,
-} from '../../../data/model/spec/spec-domain';
 import { saveSelector } from '../../../core/reducers/selector-reducers';
-import {
-  createPauseAction,
-  PauseAction,
-} from '../../../data/model/action/pause/pause';
-import { ActionValueType } from '../../../data/model/action/action-value-type';
-import { VariableType } from '../../../data/model/variable/variable-types';
 import { saveAction } from '../../../core/reducers/action-reducers';
-import { Command, createCommand } from '../../../data/model/command/command';
 import { saveCommand } from '../../../core/reducers/command-reducers';
-import {
-  createSelector,
-  createSelectorItem,
-} from '../../../data/model/selector/selector-domain';
+import { action01 } from '../../../test/resources/action-01.json';
+import { action02 } from '../../../test/resources/action-02.json';
+import { command01 } from '../../../test/resources/command-01.json';
+import { command02 } from '../../../test/resources/command-02.json';
+import { selector01 } from '../../../test/resources/selector-01.json';
+import { spec01 } from '../../../test/resources/spec-01.json';
+import { spec02 } from '../../../test/resources/spec-02.json';
+import { spec03 } from '../../../test/resources/spec-03.json';
+import { spec04 } from '../../../test/resources/spec-04.json';
+import { variable01 } from '../../../test/resources/variable-01.json';
+import { variable02 } from '../../../test/resources/variable-02.json';
+import { castJsonForTest } from '../../../test/utils/import-test-json-util';
 
-let user: UserEvent;
 // spec adequacy:
 const SPEC_WITH_SELECTOR_ID = 'spec-with-selector-id-1';
 const SPEC_WITH_SELECTOR_NAME = 'spec-with-selector-name-1';
 const SPEC_WITH_VARIABLE_ID = 'spec-with-variable-id-2';
-const SPEC_WITH_VARIABLE_NAME = 'spec-with-variable-name-2';
 // optionality:
 const SPEC_WITH_VARIABLE_OPTIONAL_ID = 'spec-with-variable-optional-id-3';
-const SPEC_WITH_VARIABLE_OPTIONAL_NAME = 'spec-with-variable-optional-name-3';
 const SPEC_WITH_VARIABLE_NOT_OPTIONAL_ID =
   'spec-with-variable-not-optional-id-4';
-const SPEC_WITH_VARIABLE_NOT_OPTIONAL_NAME =
-  'spec-with-variable-not-optional-name-4';
 // mixed
 const ROLE_KEY = 'role-key';
-const VARIABLE_ID_1 = 'non-default-variable-id-1';
 const VARIABLE_NAME_1 = 'non-default-variable-name-1';
-const DEFAULT_VARIABLE_ID_2 = 'default-variable-id-2';
 const DEFAULT_VARIABLE_NAME_2 = 'default-variable-name-2';
+
+// RTL
 const SAVE = 'Save';
 const ADD_NEW_SPEC_ITEM = 'Add New Spec Item';
 const GTE1_ERROR_MESSAGE = 'at least one spec item must be added';
+let user: UserEvent;
 
 beforeAll(() => {
-  const injected = getDefaultInjectionContext();
-  const variableMapper = injected.mappers.variable;
-
   // save variables
   // no default
-  const rangeVariable1: RangeVariable = {
-    ...createRangeVariable(),
-    id: VARIABLE_ID_1,
-    name: VARIABLE_NAME_1,
-    defaultValue: undefined,
-  };
-  const rangeVariableDTO1 = variableMapper.mapFromDomain(rangeVariable1);
-  store.dispatch(saveVariable(rangeVariableDTO1));
+  store.dispatch(saveVariable(castJsonForTest(variable01)));
   // with default
-  const variableWithDefault: RangeVariable = {
-    ...createRangeVariable(),
-    id: DEFAULT_VARIABLE_ID_2,
-    name: DEFAULT_VARIABLE_NAME_2,
-    defaultValue: 234,
-  };
-  const variableWithDefaultDTO =
-    variableMapper.mapFromDomain(variableWithDefault);
-  store.dispatch(saveVariable(variableWithDefaultDTO));
-
+  store.dispatch(saveVariable(castJsonForTest(variable02)));
   // save actions
-  const action1: PauseAction = {
-    ...createPauseAction(),
-    centiseconds: {
-      actionValueType: ActionValueType.Enum.USE_VARIABLE,
-      variableType: VariableType.Enum.NUMBER,
-      variableId: rangeVariable1.id,
-    },
-  };
-  store.dispatch(saveAction(action1));
-  const action2: PauseAction = {
-    ...createPauseAction(),
-    centiseconds: {
-      actionValueType: ActionValueType.Enum.ENTER_VALUE,
-      enteredValueType: VariableType.Enum.NUMBER,
-      value: 789,
-    },
-  };
-  store.dispatch(saveAction(action2));
+  store.dispatch(saveAction(castJsonForTest(action01)));
+  store.dispatch(saveAction(castJsonForTest(action02)));
   // save commands
-  const command1: Command = {
-    ...createCommand(),
-    specId: SPEC_WITH_SELECTOR_ID,
-    actionIds: [action2.id],
-  };
-  store.dispatch(saveCommand(command1));
-  const command2: Command = {
-    ...createCommand(),
-    specId: SPEC_WITH_VARIABLE_ID,
-    actionIds: [action1.id],
-  };
-  store.dispatch(saveCommand(command2));
+  store.dispatch(saveCommand(castJsonForTest(command01)));
+  store.dispatch(saveCommand(castJsonForTest(command02)));
 
   user = userEvent.setup();
 });
 
 beforeEach(async () => {
   /* re-saving specs before each test since some tests dirty the data */
-  const injected = getDefaultInjectionContext();
-  const selectorMapper = injected.mappers.selector;
-  const specMapper = injected.mappers.spec;
 
   // save specs
-  const selectorSpecItem: SpecItem = {
-    ...createSpecItem(),
-    itemType: SpecItemType.Enum.SELECTOR,
-    selector: {
-      ...createSelector(),
-      items: [{ ...createSelectorItem(), value: 'asdf' }],
-    },
-  };
-  const specWithSelector: Spec = {
-    id: SPEC_WITH_SELECTOR_ID,
-    name: SPEC_WITH_SELECTOR_NAME,
-    items: [selectorSpecItem],
-    roleKey: ROLE_KEY,
-    enabled: true,
-    locked: false,
-  };
-  const specDTO1 = specMapper.mapFromDomain(specWithSelector);
-  store.dispatch(saveSpec(specDTO1));
-  const selectorDTO1 = selectorMapper.mapFromDomain(selectorSpecItem.selector);
-  store.dispatch(saveSelector(selectorDTO1));
+  // spec w/ selector
+  store.dispatch(saveSpec(castJsonForTest(spec01)));
+  store.dispatch(saveSelector(castJsonForTest(selector01)));
 
   // spec w/ variable
-  const specWithVariable: Spec = {
-    id: SPEC_WITH_VARIABLE_ID,
-    name: SPEC_WITH_VARIABLE_NAME,
-    items: [
-      {
-        ...createSpecItem(),
-        itemType: SpecItemType.Enum.VARIABLE,
-        variableId: VARIABLE_ID_1,
-      },
-    ],
-    roleKey: '',
-    enabled: true,
-    locked: false,
-  };
-  const specDTO2 = specMapper.mapFromDomain(specWithVariable);
-  store.dispatch(saveSpec(specDTO2));
+  store.dispatch(saveSpec(castJsonForTest(spec02)));
 
   // spec w/ variable; optional
-  const specWithVariableOptional: Spec = {
-    id: SPEC_WITH_VARIABLE_OPTIONAL_ID,
-    name: SPEC_WITH_VARIABLE_OPTIONAL_NAME,
-    items: [
-      {
-        ...createSpecItem(),
-        itemType: SpecItemType.Enum.VARIABLE,
-        variableId: DEFAULT_VARIABLE_ID_2,
-        optional: true,
-      },
-    ],
-    roleKey: '',
-    enabled: true,
-    locked: false,
-  };
-  const specDTO3 = specMapper.mapFromDomain(specWithVariableOptional);
-  store.dispatch(saveSpec(specDTO3));
+  store.dispatch(saveSpec(castJsonForTest(spec03)));
 
   // spec w/ variable; not optional
-  const specWithVariableNotOptional: Spec = {
-    id: SPEC_WITH_VARIABLE_NOT_OPTIONAL_ID,
-    name: SPEC_WITH_VARIABLE_NOT_OPTIONAL_NAME,
-    items: [
-      {
-        ...createSpecItem(),
-        itemType: SpecItemType.Enum.VARIABLE,
-        variableId: VARIABLE_ID_1,
-        optional: false,
-      },
-    ],
-    roleKey: '',
-    enabled: true,
-    locked: false,
-  };
-  const specDTO4 = specMapper.mapFromDomain(specWithVariableNotOptional);
-  store.dispatch(saveSpec(specDTO4));
+  store.dispatch(saveSpec(castJsonForTest(spec04)));
 });
 
 const doRender = (specId?: string) => {
@@ -531,6 +407,9 @@ describe('spec component tests', () => {
   });
 
   it('should validate spec not optional + var not default', async () => {
+    // act(() => {
+    //   store.dispatch(deleteCommand(COMMAND_ID_1));
+    // });
     doRender(SPEC_WITH_VARIABLE_NOT_OPTIONAL_ID);
 
     const saveButton = screen.getByRole('button', {
