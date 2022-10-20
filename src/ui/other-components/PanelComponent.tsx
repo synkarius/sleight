399c2dc52e0a1aeb89d1b39be3e753c211fc0807
synkarius <david@explosionduck.com>
2022-10-19 21:16:50 -0700

    SLEIGHT-45: start grid screen

--- src/ui/other-components/PanelComponent.tsx
+++ src/ui/other-components/PanelComponent.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { Card } from 'react-bootstrap';
 
 interface PanelComponentProps {
   header?: string;
   children: React.ReactNode;
+  scrollable?: boolean;
 }
 
 export const PanelComponent: React.FC<PanelComponentProps> = (props) => {
+  const style: CSSProperties | undefined = props.scrollable
+    ? { overflowY: 'auto', overflowX: 'hidden' }
+    : undefined;
   return (
-    <Card className="m-2">
+    <Card className="m-2" style={style}>
       {props.header && <Card.Header>{props.header}</Card.Header>}
       <div className="m-2">{props.children}</div>
     </Card>
--- src/ui/other-components/views/CommandGridViewComponent.tsx
+++ src/ui/other-components/views/CommandGridViewComponent.tsx
@@ -1,5 +1,88 @@
-import React from 'react';
+import React, { useId, useState } from 'react';
+import { Accordion, Col, Form, FormControl, Row } from 'react-bootstrap';
+import { useAppSelector } from '../../../app/hooks';
+import { Command } from '../../../data/model/command/command';
+import { Context } from '../../../data/model/context/context';
+import { CommandParentComponent } from '../../model/command/CommandParentComponent';
+import { PanelComponent } from '../PanelComponent';
 
-export const CommandGridViewComponent: React.FC<{}> = (props) => {
-  return <></>;
+const GLOBAL_CONTEXT = 'global';
+
+const getContextKey = (contextId?: string): string => contextId ?? '';
+type GridItem = {
+  command: Command;
+  context?: Context;
+};
+const sortCommands = (a: Command, b: Command): number => {
+  return getContextKey(a.contextId) < getContextKey(b.contextId) ? -1 : 1;
+};
+const filterItem = (item: GridItem, contextSearch: string): boolean => {
+  const global =
+    !item.context &&
+    (contextSearch === '' || contextSearch.toLowerCase() === GLOBAL_CONTEXT);
+  const contextMatch = !!(
+    item.context &&
+    (item.context.name.toLowerCase().includes(contextSearch.toLowerCase()) ||
+      item.context.matcher.toLowerCase().includes(contextSearch.toLowerCase()))
+  );
+  return global || contextMatch;
+};
+
+export const CommandGridViewComponent: React.FC<{}> = () => {
+  const commands = useAppSelector((state) => state.command.saved);
+  const contexts = useAppSelector((state) => state.context.saved);
+  const [contextSearch, setContextSearch] = useState('');
+  const searchFormId = useId();
+
+  const items: GridItem[] = Object.values(commands)
+    .sort(sortCommands)
+    .map((command) => ({
+      command,
+      context: contexts[command.contextId ?? GLOBAL_CONTEXT],
+    }))
+    .filter((item) => filterItem(item, contextSearch));
+
+  const contextSearchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContextSearch(event.target.value);
+  };
+
+  return (
+    <>
+      <PanelComponent>
+        <Form.Group as={Row} className="m-1" controlId={searchFormId}>
+          <Form.Label column sm={2}>
+            Context Filter
+          </Form.Label>
+          <Col sm={10}>
+            <FormControl
+              aria-label={'context search'}
+              type="text"
+              onChange={contextSearchHandler}
+              value={contextSearch}
+              placeholder='name or matcher of context or "Global"'
+            />
+          </Col>
+        </Form.Group>
+      </PanelComponent>
+      <PanelComponent scrollable={true}>
+        <Accordion>
+          {items.map((item, index) => (
+            <Accordion.Item key={`${item.command.id}`} eventKey={`${index}`}>
+              <Accordion.Header>
+                <Col sm="2">
+                  <span>{`${item.context?.name ?? 'Global'} /`}</span>
+                </Col>
+                <Col sm="4">
+                  <span>{item.command.name}</span>
+                </Col>
+              </Accordion.Header>
+              <Accordion.Body>
+                <CommandParentComponent commandId={item.command.id} />
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      </PanelComponent>
+    </>
+  );
 };
